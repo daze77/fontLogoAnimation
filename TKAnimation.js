aedcdf86b4868f15f4e3bc789250b46323ed1e29
daze_77 <daze77@gmail.com>
2021-06-17 11:01:36 -0400

    updated the logo animation - looks smoother now

--- TKAnimation.js
+++ TKAnimation.js
@@ -46,39 +46,74 @@ function logoAnimation(){
     
     logoAnimation()
 
-    var tl = anime.timeline({
-      targets: '.logoimg',
-      delay: function(el, i) { return i * 100 },
-      duration: 400, // Can be inherited
-      easing: 'easeOutExpo', // Can be inherited
-      direction: 'alternate', // Is not inherited
-      loop: 2 // Is not inherited
-    });
+    // var tl = anime.timeline({
+    //   targets: '.logoimg',
+    //   delay: function(el, i) { return i * 100 },
+    //   duration: 450, // Can be inherited
+    //   easing: function(el, i, total) {
+    //     return function(t) {
+    //       return Math.pow(Math.sin(t * (i + 1)), total);
+    //     }
+    //   }, // Can be inherited
+    //   direction: 'alternate', // Is not inherited
+    //   loop: 2 // Is not inherited
+    // });
     
-    tl
-    .add({
-      translateY: 250,
-      translateX: 450,
-      // override the easing parameter
-      easing: 'spring',
-    })
-    .add({
-      opacity: 1,
-      scale: 2
-    })
-    .add({
-      // override the targets parameter
-      targets: '.logoimg',
-      rotate: 180
-    })
-    .add({
-      // translateX: 0,
-      // translateY:0,
-      scale: 0.75
-    })
+    // tl
+    // .add({
+    //   translateY: "15vw",
+    //   translateX: "35vw",
+    //   // override the easing parameter
+    //   easing: 'spring',
+
+      
+    // })
+    // .add({
+    //   scale: 2,
+    //   perspective: "2245px",
+    //   // skewX: 45,
+    //   // skewY: 45,
 
 
 
+    // })
+    // .add({
+    //   // override the targets parameter
+    //   targets: '.logoimg',
+
+    
+    //   rotate: "2turn"
+    // })
+    // .add({
+    //   // translateX: 0,
+    //   // translateY:0,
+    //   scale: 0.2,
+ 
+
+    // })
+
+
+    anime({
+      targets: '.tkLogoSymbolContainer .el',
+      translateX: function(el) {
+        return el.getAttribute('data-x');
+      },
+      translateY: function(el, i) {
+        return 150 + (-50 * i);
+      },
+      translateX: function(el, i) {
+        return 200 + (-50 * i);
+      },
+      scale: function(el, i, l) {
+        return (l - i) + 1;
+      },
+      rotate: function() { return anime.random(-360, 360); },
+      borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
+      duration: function() { return anime.random(2200, 2800); },
+      delay: function() { return anime.random(0, 400); },
+      direction: 'alternate',
+      loop: 2
+    });
 
 
 
@@ -104,13 +139,14 @@ function logoAnimation(){
 
 
 
-let animation = anime.timeline({loop: 2, opacity: 1})
+let animation = anime.timeline({loop: 2})
   .add({
     targets: '.tkLogoName .el',
-    translateY: ["1.1em", 0],
+    translateY: ["5.5em", 0],
     translateZ: 0,
     duration: 750,
-    delay: (el, i) => 50 * i
+    delay: (el, i) => 50 * i,
+    direction: "alternate"
   }).add({
     targets: '.tkLogoName',
     opacity: 0,
@@ -120,9 +156,9 @@ let animation = anime.timeline({loop: 2, opacity: 1})
   })
 
 
-animation.finished.then(goAgain)
+animation.finished.then(showText)
 
-function goAgain(){
+function showText(){
 
   anime.set(document.querySelector('.tkLogoName').style.opacity=1
   )
